f5ae048fd94c4ff44e5069f7fbd65fc315e39847
triangle drawer <48007646+t18b219k@users.noreply.github.com>
2023-04-26 14:53:23 +0900

    implement playground

--- examples/web_demo/src/lib.rs
+++ examples/web_demo/src/lib.rs
@@ -1,5 +1,18 @@
 use wasm_bindgen::prelude::*;
 #[wasm_bindgen(start)]
-fn main() {
-    println!("Hello, world!");
+fn main() ->Result<(),JsValue>{
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+
+
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn transpile()->Result<String,JsValue>{
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let code_area:web_sys::HtmlTextAreaElement = document.get_element_by_id("code").expect("no code pain").dyn_into::<web_sys::HtmlTextAreaElement>().unwrap();
+    
+
 }
