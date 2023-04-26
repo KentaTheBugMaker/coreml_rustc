604c16eea4a6d242c5a5bb2d07252aa96be6dd00
triangle drawer <48007646+t18b219k@users.noreply.github.com>
2023-04-26 14:53:26 +0900

    implement playground

--- examples/web_demo/src/lib.rs
+++ examples/web_demo/src/lib.rs
@@ -1,18 +1,42 @@
 use wasm_bindgen::prelude::*;
-#[wasm_bindgen(start)]
-fn main() ->Result<(),JsValue>{
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
-
-
-    Ok(())
-}
-
+use coreml_rustc::{
+    ast_transpiler::CGU,
+    flat_syntax::Dec,
+    parser_libchumsky,
+    typeinf::{self, TypeEnvironment},
+    Input, Parser,
+};
 #[wasm_bindgen]
-pub fn transpile()->Result<String,JsValue>{
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let code_area:web_sys::HtmlTextAreaElement = document.get_element_by_id("code").expect("no code pain").dyn_into::<web_sys::HtmlTextAreaElement>().unwrap();
-    
+pub fn transpile(src:String)->Result<String,JsValue>{
+    let mut type_environment = TypeEnvironment::new();
+    let mut generated = String::new();
+    for dec in src.split(';') {
+        let (tokens, _err) = parser_libchumsky::lexer()
+            .parse(&dec)
+            .into_output_errors();
+        if let Some(tokens) = tokens {
+            let (ast, errors) = parser_libchumsky::parse()
+                .parse(
+                    tokens
+                        .as_slice()
+                        .spanned((dec.len()..dec.len()).into()),
+                )
+                .into_output_errors();
+            if let Some(ast) = ast {
+                if let Ok(ty_env) = typeinf::type_inf(&type_environment, ast.clone().into())
+                {
+                    type_environment = ty_env;
+                    //型推論に成功したのでコンパイルを行う.
+                    let declaration: Dec = ast.into();
+                    let rust_declaration = declaration.generate_rust_declaration();
+                    let cgu = CGU::new(&type_environment, rust_declaration);
+                    generated.push_str(&cgu.to_string());
+                    generated.push('\n');
+                } 
+            } 
+        } 
+    }
+
+    Ok(generated)
 
 }
